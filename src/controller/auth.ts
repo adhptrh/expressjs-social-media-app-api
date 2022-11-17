8797fff3486a019ef622922cb87c440b9e459e51
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-11-17 15:55:35 +0700

    fix wrong order

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
+import Prisma from "../../prisma"
 
-function Login(res: Response, req: Request) {
+function Login(req: Request, res: Response) {
     res.send(JSON.stringify({
         "status":true,
         "message":"test"
     }))
 }
 
-function Register(res: Response, req: Request) {
+function Register(req: Request, res: Response) {
+    let lmao:RegisterRequest = req.body
+    console.log(lmao.username)
+    console.log(lmao.password)
     res.send(JSON.stringify({
         "status":true,
         "message":"test"
