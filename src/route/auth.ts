f8e91b89bc9f9fb52ca6909ff4932d65097d3600
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-11-18 01:22:11 +0700

    route for "me" api

--- src/route/auth.ts
+++ src/route/auth.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from "express"
 import * as AuthController from "../controller/auth"
 import {body} from "express-validator"
 import validateRequest from "../middleware/validate-request"
+import authenticated from "../middleware/authenticated"
 let router:Router = express.Router()
 
 router.post(
@@ -10,14 +11,21 @@ router.post(
     body("password").exists(),
     validateRequest,
     AuthController.Login
-    )
+)
+
 router.post(
     "/register", 
     body("username").isLength({min:3,max:32}),
     body("password").isLength({min:6,max:32}),
     validateRequest,
     AuthController.Register
-    )
+)
+
+router.get(
+    "/me",
+    authenticated,
+    AuthController.Me
+)
 
 
 export default router
