421f14d2a71973b78168e52c0e7857e70547ace4
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-11-18 23:49:46 +0700

    change like url path

--- src/route/post.ts
+++ src/route/post.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Router } from "express"
 import * as PostController from "../controller/post"
+import * as LikeController from "../controller/like"
 import authenticated from "../middleware/authenticated"
 let router:Router = express.Router()
 
@@ -19,6 +20,16 @@ router.delete(
     "/:id", 
     authenticated,
     PostController.Delete
+)
+router.post(
+    "/:id/like", 
+    authenticated,
+    LikeController.Create
+)
+router.delete(
+    "/:id/like",
+    authenticated,
+    LikeController.Delete
 )
 
 export default router
