9a04cafec02a53b170806557d5efe0bca6691895
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-11-18 14:57:44 +0700

    improve authenticated middleware

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import { CustomRequest } from "../type/types"
+import prismaClient from "../../prisma"
+import { User } from "../type/types"
+
 export default function(req: CustomRequest, res: Response, next: NextFunction) {
     let token = req.headers["authorization"]?.split(" ")[1]
 
@@ -11,12 +14,29 @@ export default function(req: CustomRequest, res: Response, next: NextFunction) {
         })
     }
 
-    jwt.verify(token, process.env.SECRET_TOKEN ?? "",(err:any, user:any) => {
+    jwt.verify(token, process.env.SECRET_TOKEN ?? "",async (err:any, user:any) => {
         if (err) return res.status(401).json({
             "status":false,
             "message":"Invalid jwt token"
         })
-        req.user = user
+        
+        let userfound:(User|null) = await prismaClient.user.findFirst({
+            select:{
+                id:true,
+                username:true,
+                created_at:true,
+            },
+            where:{
+                username:req.user?.username
+            }
+        })
+        if (userfound == null){
+            return res.status(401).json({
+                "status":false,
+                "message":"User not found"
+            })
+        }
+        req.user = userfound
         next()
     })
     
